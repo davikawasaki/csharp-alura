74ee563a121555e836cd52ab63141671ac592652
Davi Kawasaki <davishinjik@gmail.com>
2018-07-15 14:50:53 +0200

    Update on first project + second project

---
+++ part-1/ATMForm/ATMForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATMForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int accountNumber = 1;
+            double balance = 100.0;
+            double value = 10.0;
+            double valueAfterWithdrawl = balance - value;
+
+            MessageBox.Show("Actual balance: " + valueAfterWithdrawl);
+        }
+    }
+}
